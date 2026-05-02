531b1687dfab223c396d7bbb2cc73ac046740633
djordjevicsasa <sasa@sasadjordjevic.com>
2026-05-02 18:21:46 +0200

    Update manager.go

--- block/manager.go
+++ block/manager.go
@@ -61,3 +61,80 @@ func (m *Manager) Write(filePath string, blockIndex int, data []byte) error {
 
 	return nil
 }
+
+func (m *Manager) WriteRaw(filePath string, offset int64, data []byte) error {
+	dir := filepath.Dir(filePath)
+	if err := os.MkdirAll(dir, 0755); err != nil {
+		return fmt.Errorf("ne moze se kreirati direktorijum %s: %w", dir, err)
+	}
+
+	f, err := os.OpenFile(filePath, os.O_CREATE|os.O_RDWR, 0644)
+	if err != nil {
+		return fmt.Errorf("ne moze se otvoriti fajl %s: %w", filePath, err)
+	}
+	defer f.Close()
+
+	_, err = f.WriteAt(data, offset)
+	return err
+}
+
+func (m *Manager) ReadRaw(filePath string, offset int64, size int) ([]byte, error) {
+	f, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	buf := make([]byte, size)
+	n, err := f.ReadAt(buf, offset)
+	if err != nil && n == 0 {
+		return nil, err
+	}
+	return buf[:n], nil
+}
+
+func (m *Manager) WriteFile(filePath string, data []byte) error {
+	dir := filepath.Dir(filePath)
+	if err := os.MkdirAll(dir, 0755); err != nil {
+		return err
+	}
+	return os.WriteFile(filePath, data, 0644)
+}
+
+func (m *Manager) ReadFile(filePath string) ([]byte, error) {
+	return os.ReadFile(filePath)
+}
+
+func (m *Manager) FileSize(filePath string) (int64, error) {
+	info, err := os.Stat(filePath)
+	if err != nil {
+		return 0, err
+	}
+	return info.Size(), nil
+}
+
+func (m *Manager) Append(filePath string, data []byte) (int64, error) {
+	dir := filepath.Dir(filePath)
+	if err := os.MkdirAll(dir, 0755); err != nil {
+		return 0, err
+	}
+
+	f, err := os.OpenFile(filePath, os.O_CREATE|os.O_RDWR|os.O_APPEND, 0644)
+	if err != nil {
+		return 0, err
+	}
+	defer f.Close()
+
+	info, err := f.Stat()
+	if err != nil {
+		return 0, err
+	}
+	offset := info.Size()
+
+	_, err = f.Write(data)
+	if err != nil {
+		return 0, err
+	}
+
+	return offset, nil
+}
