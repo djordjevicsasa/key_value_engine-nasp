0cf07fba4754dd1619e4d6935bb513ff7efa9a9c
danilocortan <danilo.cortan10@gmail.com>
2026-05-05 17:44:44 +0200

    Update merkle.go
    building of merkle

--- merkle/merkle.go
+++ merkle/merkle.go
@@ -37,3 +37,46 @@ func BuildTree(data [][]byte) *MerkleTree {
 	}
 
 	levels := [][]*MerkleNode{leaves}
+	
+	// Gradimo stablo odozdo na gore
+	currentLevel := leaves
+	for len(currentLevel) > 1 {
+		var nextLevel []*MerkleNode
+
+		for i := 0; i < len(currentLevel); i += 2 {
+			// Eksplicitna alokacija da ne mutiramo postojece hash vrednosti
+			combined := make([]byte, 0, len(currentLevel[i].Hash)+len(currentLevel[i+1].Hash))
+			combined = append(combined, currentLevel[i].Hash...)
+			combined = append(combined, currentLevel[i+1].Hash...)
+			h := sha256.Sum256(combined)
+
+			parent := &MerkleNode{
+				Hash:  h[:],
+				Left:  currentLevel[i],
+				Right: currentLevel[i+1],
+			}
+			nextLevel = append(nextLevel, parent)
+		}
+
+		// Ako je neparan, dupliramo poslednji
+		if len(nextLevel) > 1 && len(nextLevel)%2 != 0 {
+			nextLevel = append(nextLevel, nextLevel[len(nextLevel)-1])
+		}
+
+		levels = append(levels, nextLevel)
+		currentLevel = nextLevel
+	}
+
+	return &MerkleTree{
+		Root:  currentLevel[0],
+		Nodes: levels,
+	}
+}
+
+// Vraca hash korena stabla kao hex string
+func (t *MerkleTree) RootHash() string {
+	if t.Root == nil {
+		return ""
+	}
+	return hex.EncodeToString(t.Root.Hash)
+}
